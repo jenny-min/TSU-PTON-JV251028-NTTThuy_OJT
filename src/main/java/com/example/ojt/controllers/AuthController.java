29360706cffa8ba6e3866e591b0dcd2298f74ccc
jenny-min <jennynguyen.92.92@gmail.com>
2026-06-17 00:37:06 +0900

    feat: add home, login and register pages with controller mappings

---
+++ src/main/java/com/example/ojt/controllers/AuthController.java
@@ -0,0 +1,21 @@
+package com.example.ojt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("auth")
+public class AuthController {
+    //Login
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/auth/login";
+    }
+
+    //Register
+    @GetMapping("/register")
+    public String registerPage() {
+        return "/auth/register";
+    }
+}
